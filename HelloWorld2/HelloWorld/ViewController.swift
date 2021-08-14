6225ef5ce1a1cbdaf511d7b42a35fb3d48b9d6ef
maropook <ii440092@icloud.com>
2021-08-15 08:29:46 +0900

    fix

---
+++ HelloWorld2/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 長谷川樹 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
